c2494c72735359c9514673d21937d9d3bf9cb90e
OmoniyIfeAbraham <ife04abraham@gmail.com>
2024-09-16 05:32:37 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+// dotenv
+require("dotenv").config();
+
+const Port = process.env.PORT || 3700;
+
+// bodyparser
+const BodyParser = require("body-parser");
+app.use(BodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(BodyParser.json({ extended: true, limit: "50mb" }));
+
+// session
+app.use(
+  require("express-session")({
+    secret: process.env.SessionSecret,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { expires: 172800000 },
+  })
+);
+
+app.use(express.static("public"));
+
+// mongoose
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose.set("runValidators", true);
+mongoose
+  .connect(process.env.mongoUri)
+  .then(() => {
+    console.log("db connected");
+    app.listen(Port, () => console.log(`http://localhost:${Port}`));
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
